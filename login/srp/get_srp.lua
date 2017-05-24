88410db8839d0a67b826a2af6b29e5b7df3307d2
Chen Tao <cantoo523@gmail.com>
2017-05-24 16:28:13 +0800

    finish srp data store functions

--- login/srp/get_srp.lua
+++ login/srp/get_srp.lua
@@ -4,24 +4,77 @@ local cmysql = require("cmysql")
 local cjson = require("cjson.safe")
 local random = require("resty.random")
 local str = require("resty.string")
+local utils = require("resty.utils")
 local srp = require("srp")
+local quote_sql_str = ngx.quote_sql_str
 
-local N_num_bits = "2048"
 local _M = {}
 
-local function set_srp_tmp(key, value)
+local N_num_bits = "2048"
+local redis_conf = {
+    host = "127.0.0.1",
+    password = "1234"
+}
+
+local mysql_conf = {
+    host = "127.0.0.1",
+    user = "root",
+    password = "1234"
+}
+
+local redis = credis:new(redis_conf)
+local mysql = cmysql:new(mysql_conf)
+local SRP_TMP_PREFIX = "SRPTMP"
+local SRP_KEY_PREFIX = "SRPKEY"
+--local SMS_FREQ_PREFIX = "SMSFREQ"
+--local SRP_FREQ_PREFIX = "SRPFREQ"
+
+local function set_srp_tmp(key, value, expire)
+    local _, err = redis:do_cmd("setex", SRP_TMP_PREFIX .. key, cjson.encode(value), expire)
+    return err
 end
 
 local function get_srp_tmp(key)
+    local data, err = redis:do_cmd("get", SRP_TMP_PREFIX .. key)
+    if err then
+        return nil, err
+    end
+
+    return type(data) == "string" and cjson.decode(data) or nil
+end
+
+local function get_srp_tb(key)
+    local hash = utils:hash(key) % 1000
+    local db, tb = math.floor(hash / 100), hash % 100
+    return string.format("dbLogin_%d.SRP_%d", db, tb)
 end
 
 local function get_srp_persist(key)
+    local sql = string.format("select v,s from %s where key=%s", 
+        get_srp_tb(key), quote_sql_str(key))
+    local data, err = mysql:get(sql)
+    if err and err == "not found" then
+        return nil
+    end
+
+    if err then
+        return nil, err
+    end
+
+    return data
 end
 
-local function del_srp_tmp(key)
-end
+local function set_srp_persist(key, value, expire)
+    local sql = string.format("update %s set v=%s,s=%s where key=%s", 
+        get_srp_tb(key), quote_sql_str(value.v), quote_sql_str(value.s), quote_sql_str(key))
+    local _, err = mysql:execute(sql)
+    if err then
+        return err
+    end
 
-local function set_srp_persist(key, value)
+    redis:do_cmd("del", SRP_TMP_PREFIX .. key)
+    _, err = redis:do_cmd("setex", SRP_KEY_PREFIX .. key, value.key, expire)
+    return err
 end
 
 local function get_arg_phone()
@@ -53,12 +106,13 @@ local function get_password(phone)
     local v = srp.create_verifier(phone, password, s, N, g)
 
     -- 写srp临时存储
-    local err = set_srp_tmp(phone, {s = s, v = v})
+    local password_valid_time = 300
+    local err = set_srp_tmp(phone, {s = s, v = v}, password_valid_time)
     if err then
         return restful:internal_server_error("获取验证码失败")
     end
 
-    -- 增加验证码发送次数
+    -- 增加验证码发送次数, 写验证码缓存
 
     -- 发送短信
     return restful:wrap({password = password, s = s})
@@ -75,9 +129,16 @@ local function get_B(phone)
         return restful:unprocessable_entity()
     end
 
-    local srp_data = get_srp_tmp(phone)
+    local srp_data, err = get_srp_tmp(phone)
+    if err then
+        return restful:internal_server_error("系统繁忙")
+    end
+
     if not srp_data then
-        srp_data = get_srp_persist(phone)
+        srp_data, err = get_srp_persist(phone)
+        if err then
+            return restful:internal_server_error("系统繁忙")
+        end
     end
 
     if not srp_data or not srp_data.v or not srp_data.s then
@@ -89,7 +150,7 @@ local function get_B(phone)
     ngx.log(ngx.DEBUG, "phone=", phone, ",b=", b, ",B=", B)
     srp_data = {v = srp_data.v, s = srp_data.s, b = b, A = A, B = B}
 
-    local err = set_srp_tmp(phone, srp_data)
+    err = set_srp_tmp(phone, srp_data)
     if err then
         return restful:internal_server_error("系统繁忙")
     end
@@ -97,7 +158,6 @@ local function get_B(phone)
     return restful:wrap({B = B})
 end
 
--- TODO: 考虑N_num_bits变了怎么办
 local function get_M2(phone)
     local client_M1 = ngx.var.arg_m1
     if not client_M1 then
@@ -120,13 +180,9 @@ local function get_M2(phone)
 
     local server_M2 = srp.Calc_M2(srp_data.A, server_M1, server_key)
     ngx.log(ngx.DEBUG, "phone=", phone, "server_M2=", server_M2)
-    local err = del_srp_tmp(phone)
-    if err then
-        ngx.log(ngx.ERR, "failed to del srp tmp,phone=", phone)
-    end
 
     srp_data = {key = server_key, v = srp_data.v, s = srp_data.s}
-    err = set_srp_persist(phone, srp_data)
+    local err = set_srp_persist(phone, srp_data)
     if err then
         return restful:internal_server_error("系统繁忙")
     end
